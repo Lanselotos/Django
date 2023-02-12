930ca8f3308a38f6dde9c354b93060a36c53c1e4
Igor Pavlov <antvr2006@mail.ru>
2023-02-12 18:19:05 +0300

    lesson15

--- new_web/settings.py
+++ new_web/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'new_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIRS = [
+    
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
